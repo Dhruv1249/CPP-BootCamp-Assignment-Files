e9086290683f68e06e5f6abc560f0ab21fd9e10b
Dhruv1249 <148534040+Dhruv1249@users.noreply.github.com>
2024-08-05 20:20:30 +0530

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+
+    int a,b;
+    cout << "Enter number a:";
+    cin >> a ;
+    cout << "Enter number b:";
+    cin >> b ;
+    cout << "a+b= "<< a+b << endl;
+    cout << "a-b= "<< a-b << endl;
+    cout << "a/b= "<< a/b << endl;
+    cout << "a%b= "<< a%b << endl;
+    cout << "a*b= "<< a*b << endl;
+
+
+}
